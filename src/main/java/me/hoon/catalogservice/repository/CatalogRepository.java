f1c308656b9c25754c2d900fc0b2fddca243acdc
leesh1993 <dltjdgnsok1@naver.com>
2022-09-28 21:27:22 +0900

    findByProductId 구현

--- src/main/java/me/hoon/catalogservice/repository/CatalogRepository.java
+++ src/main/java/me/hoon/catalogservice/repository/CatalogRepository.java
@@ -3,5 +3,8 @@ package me.hoon.catalogservice.repository;
 import me.hoon.catalogservice.domain.Catalog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CatalogRepository extends JpaRepository<Catalog, Long> {
+    Optional<Catalog> findByProductId(String productId);
 }
